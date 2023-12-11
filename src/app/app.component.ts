509fd8a5027c89190926b249013f5352dfd526fe
Giacca90 <giacca90mobile@gmail.com>
2023-12-11 14:23:16 +0100

    Añadido enrutamiento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {RouterModule} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AgregarComponent } from './agregar/agregar.component';
+import { StatusComponent } from './status/status.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterModule, HomeComponent, AgregarComponent, StatusComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'TemplateMatic';
+routeParameterValue: any;
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AgregarComponent } from './agregar/agregar.component';
+import { StatusComponent } from './status/status.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'status', component: StatusComponent },
+    { path: 'agregar', component: AgregarComponent },
+];
