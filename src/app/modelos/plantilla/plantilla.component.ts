88cb8ff84abfb922773b52870236cf2b4d4634c9
Giacca90 <giacca90mobile@gmail.com>
2023-12-29 18:30:14 +0100

    Añadida manipulación con XML en .odt (mejor que file2html)

--- src/app/modelos/plantilla/plantilla.component.ts
+++ src/app/modelos/plantilla/plantilla.component.ts
@@ -4,109 +4,158 @@ import { PlantillaService } from '../../services/plantilla.service';
 import * as file2html from 'file2html';
 import OOXMLReader from 'file2html-ooxml';
 import OdtReader from 'file2html-odf';
+import JSZip from 'jszip';
 
 file2html.config({
-  readers: [
-      OdtReader,
-      OOXMLReader
-  ]
+  readers: [OdtReader, OOXMLReader],
 });
 @Component({
   selector: 'app-plantilla',
   standalone: true,
   imports: [],
   templateUrl: './plantilla.component.html',
-  styleUrl: './plantilla.component.css'
+  styleUrl: './plantilla.component.css',
 })
-
-export class PlantillaComponent implements OnInit{
+export class PlantillaComponent implements OnInit {
   route: ActivatedRoute = inject(ActivatedRoute);
   PS: PlantillaService;
   id: number;
   file: File;
   nombre: string;
   ruta: string;
-  
-  constructor(private ps:PlantillaService) {
-    this.PS = ps;
-  
-    this.id = this.route.snapshot.queryParams['id'];
-    console.log("id: "+this.id);
-    this.file = this.ps.getPlantillaForId(this.id)
-    this.nombre = this.file.name;
-    console.log("nombre: "+this.nombre);
-    this.ruta = this.file.webkitRelativePath
-    console.log("ruta: "+this.ruta);
 
-    if(this.ruta.substring((this.ruta.length-4)).toLowerCase() == ".odt") {
+  constructor(private ps: PlantillaService) {
+    this.PS = ps;
+
+    this.id = this.route.snapshot.queryParams['id'];
+    console.log('id: ' + this.id);
+    this.file = this.ps.getPlantillaForId(this.id);
+    this.nombre = this.file.name;
+    console.log('nombre: ' + this.nombre);
+    this.ruta = this.file.webkitRelativePath;
+    console.log('ruta: ' + this.ruta);
+
+    if (this.ruta.substring(this.ruta.length - 4).toLowerCase() == '.odt') {
       this.EditOdt(this.file);
-    }else if (this.ruta.substring((this.ruta.length-4)).toLowerCase() == "docx") {
+    } else if (
+      this.ruta.substring(this.ruta.length - 4).toLowerCase() == 'docx'
+    ) {
       this.EditDocx(this.file);
     }
   }
-  ngOnInit(): void {
-   
+  ngOnInit(): void {}
+
+  buscaClaves(fileString: string) {
+    let claves: Array<string> = [];
+    let index: number = 0;
+    while (index !== -1) {
+      index = fileString.indexOf('{{', index);
+      if (index !== -1) {
+        let indexEnd = fileString.indexOf('}}', index);
+        if (indexEnd !== -1) {
+          let clave = fileString.substring(index + 2, indexEnd);
+          if (!claves.includes(clave)) {
+            claves.push(clave);
+          }
+          index = indexEnd;
+        }
+      }
+    }
+    console.log("Se han encontrado "+claves.length+" claves");
+    for(let clave of claves) {
+      console.log(clave);
+    }
   }
 
   async EditOdt(file: File) {
-    console.log("Desde EditOdt");
-    
-    try {
-        // Espera a que se resuelva la Promesa y obtén el contenido del archivo en formato ArrayBuffer
-        const content = await file.arrayBuffer();
-        
-        // Lee el archivo y conviértelo a HTML
-        const fileData = await file2html.read({
-            fileBuffer: content,
-            meta: {mimeType: "application/vnd.oasis.opendocument.text"}
-        });
+    console.log('Desde EditOdt');
+    //prueba con XML
+    const reader = new FileReader();
 
-        // Extrae los estilos y el contenido del archivo
-        const { styles, content: fileContent } = fileData.getData();
-        
-        // Concatena estilos y contenido
-        const html = styles + fileContent;
+    reader.onload = async (e: ProgressEvent<FileReader>) => {
+      // Descomprime el archivo .odt
+      const zip = await JSZip().loadAsync(this.file);
 
-        console.log("RESULTADO: \n" + html);
-    } catch (error) {
-        // Maneja cualquier error que pueda ocurrir durante el proceso
-        console.error("Error:", error);
+      // Accede al contenido del archivo content.xml
+      const contentXml = await zip.file('content.xml').async('text');
 
-        // Imprime información adicional sobre el error específico
-        if (error.code === 'file2html.errors.unsupportedFile') {
-            console.error("El formato del archivo no es compatible.");
-        }
-    }
-}
+      // Ahora puedes procesar el contenido XML como desees
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(contentXml, 'text/xml');
+      const serializer = new XMLSerializer();
+      const SxmlDoc = serializer.serializeToString(xmlDoc);
 
-  async EditDocx(file:File) {
-    console.log("Desde EditDocx");
+      console.log('Resultado XML: \n' + SxmlDoc);
+
+      this.buscaClaves(SxmlDoc);
+    };
+
+    reader.readAsArrayBuffer(file);
+
+    //prueba con file2html
     try {
       // Espera a que se resuelva la Promesa y obtén el contenido del archivo en formato ArrayBuffer
       const content = await file.arrayBuffer();
-      
+
       // Lee el archivo y conviértelo a HTML
       const fileData = await file2html.read({
-          fileBuffer: content,
-          meta: {mimeType: "application/vnd.openxmlformats-officedocument.wordprocessingml.document"}
+        fileBuffer: content,
+        meta: { mimeType: 'application/vnd.oasis.opendocument.text' },
       });
 
       // Extrae los estilos y el contenido del archivo
       const { styles, content: fileContent } = fileData.getData();
-      
+
       // Concatena estilos y contenido
       const html = styles + fileContent;
 
-      console.log("RESULTADO: \n" + html);
-  } catch (error) {
+      console.log('RESULTADO: \n' + html);
+    } catch (error) {
       // Maneja cualquier error que pueda ocurrir durante el proceso
-      console.error("Error:", error);
+      console.error('Error:', error);
 
       // Imprime información adicional sobre el error específico
       if (error.code === 'file2html.errors.unsupportedFile') {
-          console.error("El formato del archivo no es compatible.");
+        console.error('El formato del archivo no es compatible.');
       }
+    }
   }
-  
+
+  async EditDocx(file: File) {
+    console.log('Desde EditDocx');
+
+    //prueba con file2html
+    try {
+      // Espera a que se resuelva la Promesa y obtén el contenido del archivo en formato ArrayBuffer
+      const content = await file.arrayBuffer();
+
+      // Lee el archivo y conviértelo a HTML
+      const fileData = await file2html.read({
+        fileBuffer: content,
+        meta: {
+          mimeType:
+            'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+        },
+      });
+
+      // Extrae los estilos y el contenido del archivo
+      const { styles, content: fileContent } = fileData.getData();
+
+      // Concatena estilos y contenido
+      const html = styles + fileContent;
+
+      console.log('RESULTADO: \n' + html);
+
+      let view = document.getElementById('view');
+      view.innerHTML = html;
+    } catch (error) {
+      // Maneja cualquier error que pueda ocurrir durante el proceso
+      console.error('Error:', error);
+
+      // Imprime información adicional sobre el error específico
+      if (error.code === 'file2html.errors.unsupportedFile') {
+        console.error('El formato del archivo no es compatible.');
+      }
+    }
   }
 }
