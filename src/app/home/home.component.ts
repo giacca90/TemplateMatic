3360a1314e9888f9c727b2ae9408d3b877b069e4
Giacca90 <giacca90mobile@gmail.com>
2024-01-02 15:33:48 +0100

    Agregado buscador de plantillas

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,44 +1,61 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { PlantillaComponent } from '../modelos/plantilla/plantilla.component';
-import { PlantillaService, Plantilla} from '../services/plantilla.service'
+import { PlantillaService, Plantilla } from '../services/plantilla.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [RouterLink, PlantillaComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
   public plantillas: Array<Plantilla>;
-  public ps:PlantillaService;
+  public plantillasBuscadas: Array<Plantilla> = [];
+  public ps: PlantillaService;
 
-  constructor(PS:PlantillaService) {
-    this.ps=PS;
+  constructor(PS: PlantillaService) {
+    this.ps = PS;
   }
 
   ngOnInit(): void {
-     if(this.ps.getTemp()) {
+    if (this.ps.getTemp()) {
       this.plantillas = this.ps.getTemp();
-    }; 
-    let input: HTMLInputElement | null  = <HTMLInputElement | null >document.getElementById("input");
-    input.addEventListener("change", (ev:any) =>{
-      console.log("RUTA: "+input?.value);
+      this.plantillasBuscadas = this.plantillas;
+    }
+    let input: HTMLInputElement | null = <HTMLInputElement | null>(
+      document.getElementById('input')
+    );
+    input.addEventListener('change', (ev: any) => {
+      console.log('RUTA: ' + input?.value);
       this.ps.setTemp([]);
-           this.plantillas = [];  
+      this.plantillas = [];
       if (input.files) {
         for (let i = 0; i < input.files.length; i++) {
-          if(input.files[i].name.endsWith('odt') || input.files[i].name.endsWith('docx')){
-            this.plantillas.push(new Plantilla(i+1, input.files[i]));
+          if (
+            input.files[i].name.endsWith('odt') ||
+            input.files[i].name.endsWith('docx')
+          ) {
+            this.plantillas.push(new Plantilla(i + 1, input.files[i]));
           }
         }
         this.ps.setTemp(this.plantillas);
+        this.plantillasBuscadas = this.plantillas;
       }
 
-      console.log("maxiprueba: "+ this.ps.getTemp().toString());
+      console.log('maxiprueba: ' + this.ps.getTemp().toString());
     });
   }
-}
 
+  busca() {
+    let buscador: HTMLInputElement = document.getElementById('buscador') as HTMLInputElement;
+    console.log('DETECTADOS CAMBIOS EN EL BUSCADOR \n Valor: '+buscador.value);
+    this.plantillasBuscadas = [];
+    for(let plantilla of this.plantillas) {
+      if(plantilla.nombre.toLocaleLowerCase().includes(buscador.value.toLocaleLowerCase())) {
+        this.plantillasBuscadas.push(plantilla);
+      }
+    }
+  }
+}
